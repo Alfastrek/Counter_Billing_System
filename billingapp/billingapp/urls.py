005a08fdd4510dff54e51e00a7b1645d77afcc2d
Aradhya Shukla <aradhya1703@gmail.com>
2024-04-09 15:41:52 +0530

    Update urls.py

--- billingapp/billingapp/urls.py
+++ billingapp/billingapp/urls.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 urlpatterns = [
+    path('',SpectacularSwaggerView.as_view(url_name='schema'), name='index'),
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
